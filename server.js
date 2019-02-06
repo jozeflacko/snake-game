2774380222fadd20531414b0e9378b0e2335880f
Jozef Lacko <jlacko27@gmail.com>
2019-02-06 22:21:19 +0100

    add cors

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ var app = express();
 var path = require('path');
 var port = process.env.PORT || 3000;
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.static(__dirname + '/public'));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
